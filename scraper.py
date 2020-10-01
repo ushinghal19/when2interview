3c4775ff4c5ce605d24a7315f2cec9ca16776ebb
Utsav Shinghal <ushinghal19@gmail.com>
2020-10-01 23:41:18 +0800

    works for box1

--- scraper.py
+++ scraper.py
@@ -16,13 +16,22 @@ applicants = []
 
 #Getting the when2meet link
 link = "https://www.when2meet.com/?9397833-x1ZoE"
+
+#Opening the site
 driver.get(link)
 
+#Waiting for the site to load
 time.sleep(3)
 
-box1 = driver.find_element_by_id("GroupTime1595246400")
-actions.move_to_element(box1).build().perform()
 
-# applicants = driver.find_element_by_id("Available").text
-# print(applicants)
-print("test")
+box1 = driver.find_element_by_id("GroupTime1595246400")
+actions.move_to_element_with_offset(box1, 0, 0).click().perform()
+
+date = driver.find_element_by_id("AvailableDate")
+
+available = driver.find_element_by_id("Available")
+unavailable = driver.find_element_by_id("Unavailable")
+
+print(date.text)
+print(available.text)
+print(unavailable.text)
