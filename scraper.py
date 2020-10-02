884c7b5a6045076407aaa26090d66ccc5f1fa4e5
Utsav Shinghal <ushinghal19@gmail.com>
2020-10-02 13:40:53 +0800

    scraper working but too long

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 import time
 from selenium.webdriver import ActionChains
+from timeBox import timeBox
 
 #Setting up the webdriver
 driver = webdriver.Chrome()
@@ -24,14 +25,51 @@ driver.get(link)
 time.sleep(3)
 
 
-box1 = driver.find_element_by_id("GroupTime1595246400")
-actions.move_to_element_with_offset(box1, 0, 0).click().perform()
+grid = driver.find_element_by_id("GroupGridSlots")
+list_of_rows = grid.find_elements_by_xpath(".//div[@style='font-size:0px;vertical-align:top;']")
 
-date = driver.find_element_by_id("AvailableDate")
+# print(list_of_rows)
 
-available = driver.find_element_by_id("Available")
-unavailable = driver.find_element_by_id("Unavailable")
+python_grid = []
 
-print(date.text)
-print(available.text)
-print(unavailable.text)
+for row in list_of_rows:
+    boxes = []
+    boxes.extend(row.find_elements_by_tag_name("div"))
+    python_grid.append(boxes)
+
+times = {}
+
+for row in python_grid:
+    for box in row:
+        actions.move_to_element_with_offset(box, 0, 0).click().perform()
+        date = driver.find_element_by_id("AvailableDate")
+        available = driver.find_element_by_id("Available")
+        unavailable = driver.find_element_by_id("Unavailable")
+        print("date = " + date.text)
+        print("available = " + available.text)
+        print("unavailable = " + unavailable.text)
+
+
+
+# for row in grid:
+#     list_of_boxes = row.find_elements_by_tag_name("div")    
+#     python_grid.append(list_of_boxes)
+
+# print(len(python_grid))
+
+#actions.move_to_element_with_offset(box1, 0, 0).click().perform()
+
+# date = driver.find_element_by_id("AvailableDate")
+# available = driver.find_element_by_id("Available")
+# unavailable = driver.find_element_by_id("Unavailable")
+
+# t1 = timeBox(date=date.text, available=available.text, unavailable=unavailable.text, applicants=applicants, execs=execs)
+
+# print(date.text)
+# print(available.text)
+# print(unavailable.text)
+
+# print()
+# print(date.text)
+# print(available.text)
+# print(unavailable.text)
---
+++ timeBox.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from dateutil import parser
+
+parser.parse("Aug 28 1999 12:00AM")  # datetime.datetime(1999, 8, 28, 0, 0)
+
+class timeBox:
+    """ Represents one box of time in the when2meet"""
+
+    date: datetime
+    available: list
+    unavailable: list
+    num_of_execs: int
+    num_of_applicants: int
+
+    def __init__(date, available, unavailable, execs, applicants):
+        this.date = parser.parse(date)
+        this.available = available
+        this.unavailable = unavailable
+        this.num_of_applicants = len(applicants)
+        this.num_of_execs = len(execs)
